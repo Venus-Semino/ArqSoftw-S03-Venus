3c001c35c59892ceb2152fc34e39a097515c2f29
venus semino <venus.semino@tecdesoftware>
2026-05-22 21:51:32 -0600

    CREATE: Registro y CuentaController

---
+++ Catalogo.Presentation/Controllers/CuentaController.cs
@@ -0,0 +1,41 @@
+﻿namespace Catalogo.Presentation.Controllers
+using CatalogoApp.Domain.Interfaces;
+using CatalogoApp.Domain.Models;
+using global::CatalogoApp.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogoApp.Presentation.Controllers
+{
+    public class CuentaController : Controller
+    {
+        private readonly IUsuarioRepository _usuarioRepo;
+
+        public CuentaController(IUsuarioRepository usuarioRepo)
+        {
+            _usuarioRepo = usuarioRepo;
+        }
+
+        [HttpGet]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registro(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _usuarioRepo.Registrar(usuario);
+                // Redirigir al catálogo después de registrarse con éxito
+                return RedirectToAction("Index", "Catalogo");
+            }
+            return View(usuario);
+        }
+    }
+}
+{
+    public class CuentaController
+    {
+    }
+}
